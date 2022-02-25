5cc31e94f861a376f7b97deaf995ddfdc5a32145
Kleiton Pereira <kleitonrpereira@gmail.com>
2022-02-25 15:44:21 -0300

    Desafio 05 concluido

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,3 @@
-/**
- * Required External Modules
- */
-
 import * as dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
@@ -9,35 +5,57 @@ import helmet from "helmet";
 
 dotenv.config();
 
-/**
- * App Variables
- */
-
 if (!process.env.PORT) {
 	process.exit(1);
 }
 
 const PORT: number = parseInt(process.env.PORT as string, 10);
-
 const app = express();
-
-/**
- *  App Configuration
- */
 
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-/**
- * Server Activation
- */
-
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
 
+	class Aluno {
+		nome;
+		idade;
+		nota;
 
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
-	
+		constructor(nome: string, idade: number, nota: number) {
+			this.nome = nome;
+			this.idade = idade;
+			this.nota = nota;
+		}
+	}
+
+	let aluno1 = new Aluno("Maria", 30, 8.5);
+	let aluno2 = new Aluno("Roberto", 30, 9.3);
+	let aluno3 = new Aluno("Kleiton", 25, 9.5)
+
+	const alunos: Array<Aluno> = [];
+	alunos.push(aluno1, aluno2, aluno3);
+
+	let somaNotas = 0;
+	let somaIdades = 0;
+
+	for (let i = 0; i < alunos.length; i++) {
+		
+		somaNotas = somaNotas + alunos[i].nota;
+		somaIdades = somaIdades + alunos[i].idade;
+	}
+
+	const somaAlunos = new Aluno("Somas", somaIdades, somaNotas)
+	alunos.push(somaAlunos);	
+
+	const ObjectToCsv = require('objects-to-csv');
+ 
+	(async () => {
+		const csv = new ObjectToCsv(alunos);
+		await csv.toDisk('./arquivo.csv');
+		
+	})();
 });
+ 
